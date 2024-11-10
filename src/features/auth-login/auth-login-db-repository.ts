1c6ba89700d1f0cbe68c817c91e3d093c62a0cc0
alexandr2075 <gagara5620@gmail.com>
2024-11-10 15:25:23 +0900

    refactor

--- src/features/auth-login/auth-login-db-repository.ts
+++ src/features/auth-login/auth-login-db-repository.ts
@@ -1,17 +1,17 @@
 import {client} from "../../db/dbMongo";
 import {SETTINGS} from "../../settings";
 import {UserInputDBModel} from "../../models/usersModels";
-import {genHashPassword, matchPasswords} from "../../helpers/genHashPassword";
+import {matchPasswords} from "../../helpers/genHashPassword";
 
 export const authLoginRepository = {
 
     async authLoginUser(loginOrEmail: string, password: string) {
         const result = await client.db(SETTINGS.DB_NAME)
             .collection<UserInputDBModel>('users').findOne({$or: [{login: loginOrEmail}, {email: loginOrEmail}]})
-        console.log('result', result)
-        const hashPassword = await genHashPassword(password);
-        return await matchPasswords(password, hashPassword)
-
+        if (result) {
+            return await matchPasswords(password, result.password)
+        }
+        return false
     },
 
 }
